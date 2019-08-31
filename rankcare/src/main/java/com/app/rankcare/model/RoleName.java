a3d40b7116939be2edcde8c8a9e565a894aa0ea7
Ken Nguyen <ken@vivuapp.com>
2019-08-31 00:56:17 -0700

    Initial Commit

---
+++ rankcare/src/main/java/com/app/rankcare/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.app.rankcare.model;
+
+public enum RoleName {
+    ROLE_CLIENT,
+    ROLE_ADMIN
+}
---
+++ rankcare/src/main/java/com/app/rankcare/model/User.java
@@ -0,0 +1,122 @@
+package com.app.rankcare.model;
+
+import org.hibernate.annotations.NaturalId;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+            "username"
+        }),
+        @UniqueConstraint(columnNames = {
+            "email"
+        })
+})
+public class User extends DateAudit {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+    
+    @Size(max = 15)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName role;
+
+    public User() {
+
+    }
+
+    public User(Long id, @NotBlank @Size(max = 40) String name, @NotBlank @Size(max = 15) String username,
+            @NotBlank @Size(max = 40) @Email String email, @NotBlank @Size(max = 100) String password,
+            @Size(max = 15) String phoneNumber, RoleName role) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public RoleName getRole() {
+        return role;
+    }
+
+    public void setRole(RoleName role) {
+        this.role = role;
+    }
+}

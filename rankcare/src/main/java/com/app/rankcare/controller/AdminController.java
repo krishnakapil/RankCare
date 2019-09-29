6a06583c791af1cd1f86ae3ef333faaab8c52ba6
AmrithaSwetha <54731907+AmrithaSwetha@users.noreply.github.com>
2019-09-29 21:38:56 +0530

    toxicity pagination and sorting

--- rankcare/src/main/java/com/app/rankcare/controller/AdminController.java
+++ rankcare/src/main/java/com/app/rankcare/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.app.rankcare.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,11 +8,17 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.rankcare.model.Toxicity;
+import com.app.rankcare.payload.ToxicityRequest;
 import com.app.rankcare.repository.ToxicityRepository;
 
 @RestController
@@ -41,5 +48,19 @@ public class AdminController {
 	    }
 	    return chemicalMap;
 	 }
+	 @GetMapping("/getToxicityPagination")			
+	 public Map<String,Object> getToxicityPagination(@RequestParam(defaultValue="0") Integer pageNo,@RequestParam(defaultValue="10") Integer pageSize,@RequestParam(defaultValue="id") String sortBy) {		
+		 Map<String,Object> resMap = new HashMap<String,Object>();
+		 Pageable pagination= PageRequest.of(pageNo,pageSize,Sort.by(sortBy));
+		 resMap.put("pageCnt",0);
+		 resMap.put("data",new ArrayList<Toxicity>());
+		 Page<Toxicity> pgLst=toxicityRepository.findAll(pagination);
+		 if(pgLst.hasContent()) {	
+			 resMap.put("pageCnt",pgLst.getTotalPages());
+			 resMap.put("data",pgLst.getContent());
+		 }
+	    return resMap;
+	 }
+	 
 
 }
--- rankcare/src/main/java/com/app/rankcare/repository/ToxicityRepository.java
+++ rankcare/src/main/java/com/app/rankcare/repository/ToxicityRepository.java
@@ -1,5 +1,7 @@
 package com.app.rankcare.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.app.rankcare.model.Toxicity;
 @Repository
 public interface ToxicityRepository extends JpaRepository<Toxicity, Long> {
 
+	List<Toxicity> findAll();
 }

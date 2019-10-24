c139b1b15bc2a9c5a6180ba7165568676fc6d785
Ken Nguyen <ken@vivuapp.com>
2019-10-24 00:36:40 -0700

    Updates

---
+++ precache-manifest.63d8a8dddeb24889b7ffb7cbdc6cb516.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ee09fafee34d3f0b4ce07591feef38",
+    "url": "/rankcare-client/index.html"
+  },
+  {
+    "revision": "f40349aaf65509f9d7b6",
+    "url": "/rankcare-client/static/css/2.09496823.chunk.css"
+  },
+  {
+    "revision": "e07e131cfa9211091c34",
+    "url": "/rankcare-client/static/css/main.97b1fdfc.chunk.css"
+  },
+  {
+    "revision": "f40349aaf65509f9d7b6",
+    "url": "/rankcare-client/static/js/2.cac407d6.chunk.js"
+  },
+  {
+    "revision": "e07e131cfa9211091c34",
+    "url": "/rankcare-client/static/js/main.9decd73a.chunk.js"
+  },
+  {
+    "revision": "662c5268137832b371fc",
+    "url": "/rankcare-client/static/js/runtime~main.09430ab2.js"
+  },
+  {
+    "revision": "f3669c9131a23ef3d8528a1f57f9faf9",
+    "url": "/rankcare-client/static/media/crc_logo.f3669c91.png"
+  },
+  {
+    "revision": "240c7548744a413800fac72b7afbb671",
+    "url": "/rankcare-client/static/media/header_bg.240c7548.png"
+  },
+  {
+    "revision": "be5ea3ab0c1e997bc82083318197cc93",
+    "url": "/rankcare-client/static/media/logo.be5ea3ab.png"
+  }
+]);
